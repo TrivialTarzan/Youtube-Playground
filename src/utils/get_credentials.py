a7406166ef99e02a35bdd93493c2e4caacf2c6f9
TrivialTarzan <m19955991s@gmail.com>
2024-11-05 11:21:06 +0100

    add a return hint

--- src/utils/get_credentials.py
+++ src/utils/get_credentials.py
@@ -1,7 +1,7 @@
 import json
 
 
-def get_credentials(filepath="credentials.json"):
+def get_credentials(filepath="credentials.json") -> list:
     with open(filepath, "r") as file:
         credentials = json.load(file)
         login = credentials.get("login")
