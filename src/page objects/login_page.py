4d93ddde6cdbe296aab2c39e4ffa95ae88cc72b4
TrivialTarzan <m19955991s@gmail.com>
2024-11-05 10:42:24 +0100

    created login_page class

--- src/page objects/login_page.py
+++ src/page objects/login_page.py
@@ -1,13 +1,25 @@
-import selenium
 from seleniumpagefactory import PageFactory
+
 
 class LoginPage(PageFactory):
 
-    def __init__(self, driver):
+    def __init__(self, driver, user_name, password):
+        super().__init__()
         self.driver = driver
+        self.user_name = user_name
+        self.password = password
 
     locators = {
-        'user_name': ('name', "login"),
-        'password': ('name', 'password'),
+        'user_name_input': ('name', "login"),
+        'password_input': ('name', 'password'),
         'login_btn': ('button', 'submit')
     }
+
+    def enter_user_name(self):
+        self.user_name_input.set_text(self.user_name)
+
+    def enter_password(self):
+        self.password_input.set_text(self.password)
+
+    def click_login(self):
+        self.login_btn.click()
