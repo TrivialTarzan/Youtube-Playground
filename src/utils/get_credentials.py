58d0738a657ba524f10ab836bba25f8553cf7c15
TrivialTarzan <m19955991s@gmail.com>
2024-11-05 11:18:48 +0100

    added a function that gets credentials from a file included in gitignore

---
+++ src/utils/get_credentials.py
@@ -0,0 +1,10 @@
+import json
+
+
+def get_credentials(filepath="credentials.json"):
+    with open(filepath, "r") as file:
+        credentials = json.load(file)
+        login = credentials.get("login")
+        password = credentials.get("password")
+    credentials = [login, password]
+    return credentials
