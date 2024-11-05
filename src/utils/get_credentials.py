c606b3a5e9b7ee9a7f1fed27e74289cd0948a73d
TrivialTarzan <m19955991s@gmail.com>
2024-11-05 11:36:33 +0100

    added a project related info

--- src/utils/get_credentials.py
+++ src/utils/get_credentials.py
@@ -1,5 +1,8 @@
 import json
 
+# Credentials are loaded from a JSON file that is excluded from the project.
+# To use this code, you must sign up for the Allegro sandbox website and provide your own credentials
+
 
 def get_credentials(filepath="credentials.json") -> list:
     with open(filepath, "r") as file:
