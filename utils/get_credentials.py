430483319ff8e83bcef18d87c532954eedd5b015
TrivialTarzan <m19955991s@gmail.com>
2024-11-09 15:05:18 +0100

    added a comment

--- utils/get_credentials.py
+++ utils/get_credentials.py
@@ -1,9 +1,7 @@
 import json
 
-# Credentials are loaded from a JSON file that is excluded from the project.
-# To use this code, you must sign up for the Allegro sandbox website and provide your own credentials
 
-
+# Credentials are loaded from a JSON file that is included in .gitignore
 def get_credentials(filepath="credentials.json") -> list:
     with open(filepath, "r") as file:
         credentials = json.load(file)
